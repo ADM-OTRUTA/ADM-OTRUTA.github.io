8e61d50d01c1656a58030ee80c830003e6c7093c
Svynoslav <green888cat@gmail.com>
2024-11-13 04:10:25 +0200

    demo

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
     },
     root: "src",
     build: {
-      sourcemap: true,
+      sourcemap: false,
 
       rollupOptions: {
         // input: glob.sync("./src/*.html"),
